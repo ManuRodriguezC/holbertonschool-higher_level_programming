6b9935fcd2caefa8ced4bc33edb86d0fc7575cb1
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-21 19:47:29 -0500

    prints the number of and the list of its arguments.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     
     size = len(argv)
-    print("{} arguments.".format(size - 1))
+    print("{} arguments:".format(size - 1))
     for count in range(1, size):
         print("{}: {}".format(count, argv[count]))
 
