7f0c4b1e68c1b47fd2b7da677f86092fa7b1efc1
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-21 20:11:30 -0500

    prints the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     size = len(argv)
-    result = 0
-    for add in range(1, size):
-        result = result + int(argv[add])
-    print(total)
+    add = 0
+    for pos in range(1, size):
+        add = add + int(argv[pos])
+    print(add)
