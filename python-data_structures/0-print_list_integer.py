14c022dca8df6ffef536efe2601d48bd73d88b05
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-23 16:48:06 -0500

    I learning to the diferentes types of the dates in python, in this case is list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     count = 0
     size = len(my_list)
     while count < size:
-        print("{}".format(my_list[count]))
+        print("{}".format(int(my_list[count])))
         count += 1
