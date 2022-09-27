7a09c1b4a3839aa42fed75173f9ef60aadbb89ac
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-26 21:36:33 -0500

    Technical interview preparation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,12 +7,24 @@ def roman_to_int(roman_string):
         if roman_string[x] == 'I':
             value += 1
         elif roman_string[x] == 'V':
+            if x > 0 and roman_string[x - 1] == 'I':
+                value -= 2
             value += 5
         elif roman_string[x] == 'X':
+            if x > 0 and roman_string[x - 1] == 'I':
+                value -= 2
+            elif x > 0 and roman_string[x - 1] == 'V':
+                10
             value += 10
         elif roman_string[x] == 'L':
             value += 50
         elif roman_string[x] == 'C':
+            if x > 0 and roman_string[x - 1] == 'I':
+                value -= 2
+            elif x > 0 and roman_string[x - 1] == 'V':
+                value -= 10
+            elif x > 0 and roman_string[x - 1] == 'X':
+                value -= 20
             value += 100
         elif roman_string[x] == 'D':
             value += 500
