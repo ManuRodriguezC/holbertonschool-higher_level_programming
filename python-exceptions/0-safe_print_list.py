0be4320b4e9009574b3581371625f780b126a1b5
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-27 16:59:21 -0500

    Function taht print x elements of a list, uses try and except functions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
         try:
-            for i in range(len(my_list)):
+            for i in my_list:
                 if i < x:
                     print(my_list[i], end="")
             print()
         except IndexError:
             print()
         finally:
-            if x > len(my_list):
-                return len(my_list)
+            if x > i:
+                return i
             else:
                 return x
