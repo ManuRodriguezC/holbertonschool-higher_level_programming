2d3a7521851fcf64134bd12a1f3eb5b9b93921a4
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-27 20:37:06 -0500

    Function taht print x elements of a list, uses try and except functions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             if x == 0:
                 return 0
+            num = 0
             while num < x:
                 print(my_list[num], end="")
                 num += 1
