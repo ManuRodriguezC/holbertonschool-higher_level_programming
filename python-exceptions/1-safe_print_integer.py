afd2e85c9309f7c187cb4125c561e1150ec932d9
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-27 20:52:02 -0500

    function that prints an integer with "{:d}".format(). uses try: and except functions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        print("School is not an integer")
-        return False
-    finally:
         return True
+    except:
+        return False
