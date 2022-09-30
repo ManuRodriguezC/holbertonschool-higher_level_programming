8a4789028c2cba2cb44ab5249c31788e7e3a50be
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-29 19:55:20 -0500

    This funtion contains the class and methos of the class, thos method calculate the square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-""""""
+"""Create a square class"""
+
 
 class Square:
     def __init__(self, size=0):
+        """
+        Chack if the size is a integer type, if size is
+        not integrer print a error with exception raise
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
+    """This method calculate the square are if size is integer"""
     def area(self):
         a = self.__size * self.__size
         return a
