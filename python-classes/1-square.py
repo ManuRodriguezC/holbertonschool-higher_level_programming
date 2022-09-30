5434ee1511f6fed7abaa219dc4fdf69b79e4c198
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-29 19:15:45 -0500

    Class square that defines a saquare with one attibute

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Create the square class"""
+class Square:
+    """Square class is initialized with one attribute"""
+    def __init__(self, size):
+        """Size is a private attribute"""
+        self.__size = size
