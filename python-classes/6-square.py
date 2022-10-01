3f7b8f8c07674228bbae40f1c60d3cd6f1766cb8
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-30 19:14:23 -0500

    Program that print the square in specific position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,9 +35,9 @@ class Square:
     @position.setter
     def position(self, value):
         """This method modify and check the value of the position"""
-        if type(value[0]) != int or type(value[1]) != int:
+        if type(value[0]) != int and type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
