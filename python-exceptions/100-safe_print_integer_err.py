bb0c96ca75b036badcd4082070399618a007499b
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-02 14:17:57 -0500

    function that prints an integer

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,9 @@ def safe_print_integer_err(value):
         if value + 1:
             print("{:d}".format(value))
             return True
-    except (ValueError, TypeError) as exc:
-        print("Exception: {exc}".format(file=sys.stderr))
+    except:
+        if value + "":
+            sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str'\n")
+        elif value + 1.1111:
+            sys.stderr.write("Exception: Unknown format code 'd' for object of type 'float'\n")
         return False
