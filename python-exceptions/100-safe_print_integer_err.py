c8d0accad76e6114a046416780f67f0a828f1e5c
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-02 14:26:46 -0500

    function that prints an integer

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,10 @@ def safe_print_integer_err(value):
         if value + 1:
             print("{:d}".format(value))
             return True
-    except (ValueError, TypeError) as exc:
+    except TypeError as exc:
         sys.stderr.write("Exception: " + str(exc) + "\n")
         return False
+    except ValueError as exc:
+        sys.stderr.write("Exception: " + str(exc) + "\n")
+        return False
+    
