79210a5fb7ba3e5635949a4d85904b650fad5216
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 11:50:53 -0500

    Learning about TDD

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("test/0-add_integer.txt")
