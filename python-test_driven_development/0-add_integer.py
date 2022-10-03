b8b77596a0d4856a645c821bdfe283db7ad22600
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 13:00:09 -0500

    function that adds 2 integers and create the test with doctest

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""Module"""
+
+
 def add_integer(a, b=98):
+    """
+    This function return add of a and b, where a
+    and b need to int or floats numbers
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
     else:
-        return a + b
-
+        return int(a + b)
+"""Check doctest of the module"""
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("test/0-add_integer.txt")
+    doctest.testfile("tests/0-add_integer.txt")
