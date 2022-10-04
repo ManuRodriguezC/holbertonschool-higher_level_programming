47e21d27e2ef6172183ffe01dc767b7b6678d74f
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-04 12:17:15 -0500

    Print a string and split with take a . and ?, and check if text is string

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Module: 5-text_indentation"""
 
+
 def text_indentation(text):
     """check if text is string"""
     if type(text) != str:
         raise TypeError("text must be a string")
 
     for i in range(len(text)):
-        if text[i] == "." or text[i] == "?":
+        if text[i] == ":" or text[i] == "?" or text[i] == ".":
             print(text[i])
             print()
-        if text[i] != "." and text[i] != "?":
+        if text[i] != ":" and text[i] != "." and text[i] != "?":
             print(text[i], end="")
 
 if __name__ == "__main__":
