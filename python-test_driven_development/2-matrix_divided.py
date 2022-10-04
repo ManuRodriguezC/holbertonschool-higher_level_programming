13c1aeaf74ab1039b140e68c04363d568ada8123
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 19:12:24 -0500

    function taht divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,20 +8,20 @@ def matrix_divided(matrix, div):
     size = "Each row of the matrix must have the same size"
     num = "div must be a number"
     zero = "division by zero"
-    
+
     if type(matrix) != list:
         raise TypeError(not_list)
-    
+
     if (type(matrix[0]) != list) and (type(matrix[1]) != list):
         raise TypeError(not_list)
-    
+
     if len(matrix) != 1:
         if len(matrix[0]) != len(matrix[1]):
             raise TypeError(size)
-    
+
     if type(div) != int and type(div) != float:
         raise TypeError(num)
-    
+
     if div == 0:
         raise ZeroDivisionError(zero)
     new = []
