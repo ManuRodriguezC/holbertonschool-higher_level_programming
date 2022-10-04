77a3636e43c7cc8c61c2b96d6ee6e319600680bf
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 20:51:45 -0500

    Function that check and return the add of two numbers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,16 +11,11 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    if a is None or (type(a) is not int and type(a) is not float):
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
     if type(a) == float:
         a = int(a)
     if type(b) == float:
         b = int(b)
-    else:
-        return a + b
+    return a + b
 """Check doctest of the module"""
 if __name__ == "__main__":
     import doctest
