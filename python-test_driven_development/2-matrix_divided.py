f26896ff3615ec2c9835da35d357b26d64a9fa1d
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 19:11:20 -0500

    function taht divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,27 +4,37 @@
 
 def matrix_divided(matrix, div):
     """this function"""
+    not_list = "matrix must be a matrix (list of lists) of integers/floats"
+    size = "Each row of the matrix must have the same size"
+    num = "div must be a number"
+    zero = "division by zero"
+    
     if type(matrix) != list:
-        raise TypeError(" must be a matrix (list of lists) of integers/floats")
+        raise TypeError(not_list)
+    
     if (type(matrix[0]) != list) and (type(matrix[1]) != list):
-        raise TypeError(" must be a matrix (list of lists) of integers/floats")
+        raise TypeError(not_list)
+    
     if len(matrix) != 1:
         if len(matrix[0]) != len(matrix[1]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(size)
+    
     if type(div) != int and type(div) != float:
-        raise TypeError("div must be a number")
+        raise TypeError(num)
+    
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(zero)
     new = []
     new_list = []
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(not_list)
             new.append(round(matrix[i][j] / div, 2))
         new_list.append(new)
         new = []
     return new_list
+
 """check doctest to module"""
 if __name__ == "__main__":
     import doctest
