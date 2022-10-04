3a897a6257da54b4386f345961d2a36012417694
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 19:04:47 -0500

    function taht divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,27 +4,36 @@
 
 def matrix_divided(matrix, div):
     """this function"""
-    if type(matrix) != list:
-        raise TypeError(" must be a matrix (list of lists) of integers/floats")
-    if (type(matrix[0]) != list) and (type(matrix[1]) != list):
-        raise TypeError(" must be a matrix (list of lists) of integers/floats")
-    if len(matrix) == 2:
-        if len(matrix[0]) != len(matrix[1]):
-            raise TypeError("Each row of the matrix must have the same size")
-    if type(div) != int and type(div) != float:
-        raise TypeError("div must be a number")
+
+    zero = "division by zero"
+    num = "div must be a number"
+    size = "Each row of the matrix must have the same size"
+    not_list = "matrix must be a matrix (list of lists) of integers/floats"
+
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(zero)
+    
+    if type(div) != int and type(div) != float:
+        raise TypeError(num)
+    
+    if type(matrix) != list:
+        raise TypeError(not_list)
+
     new = []
     new_list = []
     for i in range(len(matrix)):
+        if type(matrix[i]) != list:
+            raise TypeError(not_list)
+        if len(matrix[i]) != len(matrix[0]):
+            raise TypeError(size)
         for j in range(len(matrix[i])):
-            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
-                raise TypeError("matrix must be a matrix(list of lists) of integers/floats")
+            if not isinstance(matrix[i][j](int, float)):
+                raise TypeError(not_list)
             new.append(round(matrix[i][j] / div, 2))
         new_list.append(new)
         new = []
     return new_list
+
 """check doctest to module"""
 if __name__ == "__main__":
     import doctest
