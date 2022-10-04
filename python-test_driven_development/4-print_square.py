8eecbb1fe1ce72c01ebe3dcd5f8e3bf532436f1e
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-03 21:51:16 -0500

    Print the square and ckeck if size is int

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Module: 4-print_square"""
 
 
 def print_square(size):
+    """
+    Checkd if size is int and if >= 0
+    In the case taht size cumpled to required, print the square,
+    wiht size = size.
+    """
     error_type = "size must be an integer"
     error_size = "size must be >= 0"
+
     if type(size) == float and size < 0:
         raise TypeError(error_type)
 
