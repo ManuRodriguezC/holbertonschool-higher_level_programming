f205f9b7d408dd5d3553e4202ab303530369b5b1
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-05 16:44:06 -0500

    Created the Rectangle class with two class  attribute

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -31,6 +31,6 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise TypeError("height must be >= 0 ")
         else:
             self.__height = value
