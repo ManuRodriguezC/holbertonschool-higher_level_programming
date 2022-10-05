34f2ad06986da5eb95778271bfbe14b40f2f911d
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-05 10:30:57 -0500

    Learning about the unittests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,5 +11,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_int_negative(self):
         self.assertTrue([-2, 3, -44, 20], 20)
 
+    def max_at_the_end(self):
+        self.assertTrue([20, 3, 22, 122], 122)
+
 if __name__ == "__main__":
     unittest.main
