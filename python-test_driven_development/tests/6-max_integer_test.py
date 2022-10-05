85bb108aad79c08d76323fc1c5337641610f2a27
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-05 12:42:32 -0500

    Learning about the unittests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,7 +6,7 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-
+    """test with unittest a max_integer function"""
     def test_doc(self):
         test = __import__('6-max_integer').__doc__
         self.assertTrue(len(test) > 1)
@@ -16,29 +16,37 @@ class TestMaxInteger(unittest.TestCase):
         self.assertTrue(test > 1)
 
     def test_int(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        lists = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer(lists), 5)
 
     def max_beginning(self):
-        self.assertEqual(max_integer([80, 70, 60, 50, 40]), 80)
+        lists = [80, 70, 60, 50, 40]
+        self.assertEqual(max_integer(lists), 80)
 
     def max_middle(self):
-        self.assertEqual(max_integer([10, 20, 30, 15, 12]), 30)
+        lists = [10, 20, 30, 15, 12]
+        self.assertEqual(max_integer(lists), 30)
 
     def test_int_negative(self):
-        self.assertEqual(max_integer([2, 3, -44, 20]), 20)
+        lists = [2, 3, -44, 20, 10]
+        self.assertEqual(max_integer(lists), 20)
 
     def max_only_negative(self):
-        self.assertEqual(max_integer([-10, -30- -5, -20]), -5)
+        lists = [-10, -30- -5, -20, -23]
+        self.assertEqual(max_integer(lists), -5)
 
     def max_one_element(self):
-        self.assertEqual(max_integer(4), 4)
+        lists = [4]
+        self.assertEqual(max_integer(lists), 4)
 
     def max_list_empty(self):
-        self.assertIsNone(max_integer([]))
+        lists = []
+        self.assertIsNone(max_integer(lists))
 
     def test_node(self):
         with self.assertRaises(TypeError):
             max_integer(None)
+
 
 if __name__ == "__main__":
     unittest.main
