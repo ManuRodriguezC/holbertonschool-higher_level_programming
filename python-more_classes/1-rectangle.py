b07de1a5290674904398bf4c4cd51b828ff25d27
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-05 16:43:08 -0500

    Created the Rectangle class with two class  attribute

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise TypeError("width must be >= 0 ")
         else:
             self.__width = value
 
