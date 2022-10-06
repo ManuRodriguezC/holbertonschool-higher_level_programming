753db114b6bec5dc2ef413097fcff0b65c2f247f
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-05 21:08:36 -0500

    Print the rectangle with __str__ and __repr

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("eidth must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__heigh
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        rect = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rect += "#"
+            if i != (self.__height - 1):
+                rect += "\n"
+        return rect
