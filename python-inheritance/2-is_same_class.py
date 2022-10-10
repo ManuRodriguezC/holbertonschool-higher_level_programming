de9bf3048e2fb56614b464e6fabf721e43a5dc62
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-10 18:43:41 -0500

    check if obj is instance of the class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This function return True if obj is same a_class if not return False"""
+"""This module have a function is_same_class"""
 
 
 def is_same_class(obj, a_class):
+    """This function return True if obj is  a_class if not return False"""
     if isinstance(obj, a_class):
         return True
     else:
