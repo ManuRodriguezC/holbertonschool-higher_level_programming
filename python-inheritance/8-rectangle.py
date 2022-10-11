c382a22b3b0ea2845d29a3c36f262fffa0f1b0e1
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-11 15:24:59 -0500

    Create the rectangle, inherits from basegeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,5 +8,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         super().integer_validator("width", width)
         super().integer_validator("height", height)
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
