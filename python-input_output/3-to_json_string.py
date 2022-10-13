e36fec1b5cf929ee89d1c4c55fcb6546a045ae99
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-13 13:18:30 -0500

    Representation in json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module json"""
+import json
+
+
+def to_json_string(my_obj):
+    """This method return the representation of the object in json"""
+    return json.dumps(my_obj)
