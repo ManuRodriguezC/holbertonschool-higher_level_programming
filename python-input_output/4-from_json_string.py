7f43a2978bb5bf001d1a5ba760b9316b585bf646
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-13 13:26:46 -0500

    from json to python

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module:json-python"""
+import json
+
+
+def from_json_string(my_str):
+    """This methos convert the representatin json a object python"""
+    return json.loads(my_str)
