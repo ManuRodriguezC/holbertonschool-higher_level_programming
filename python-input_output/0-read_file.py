89e3029df7793741a1fb065ec37e4c0276ae3557
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-13 12:32:49 -0500

    Create the methos that read a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """This methos open and read a file"""
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
