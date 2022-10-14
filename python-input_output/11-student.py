7f2a51798aaac8f367c8078c154082fde2eb34be
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-14 11:53:08 -0500

    Add tasks 8 and 11 implement the class with json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module: Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """This method inicializate the atribbutes of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This method return a json representation of a class"""
+        if attrs is not None:
+            return ({item: self.__dict__[item] for item
+                     in attrs if item in self.__dict__})
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """This method replaces all attributes of the Student"""
+        for attribute in json:
+            self.__dict__[attribute] = json[attribute]
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def class_to_json(obj):
+    """This function return the dictionary with simple data structure"""
+    return obj.__dict__
