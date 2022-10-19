651ee9ea2ad533352d6ba982f1b25688e5eb0cb0
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-19 09:58:23 -0500

    Create the repository of the class and inherete

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module Base"""
+
+
+class Base:
+    """Attributes of the class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Method that inicilizate the objects"""
+        self.id = id
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        if value == None:
+            Base.__nb_objects += 1
+            self.__id = Base.__nb_objects
+        else:
+            self.__id = value
