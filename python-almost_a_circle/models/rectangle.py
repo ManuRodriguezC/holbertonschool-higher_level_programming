03340543a790cb0f0e015bcaf4ceebeb9e1d1eec
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-19 11:08:07 -0500

    This module inherete of the base and create new attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,14 +33,15 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("wigth must be > 0")
-        self.__width = value
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
