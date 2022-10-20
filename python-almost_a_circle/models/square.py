34dcca210b7254f5795a97d83ed541aa79a873d4
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-19 19:58:15 -0500

    Create the class Square, this class inherits of Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """Module: Square"""
+from pyrsistent import v
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Class Square that inherete of the Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """This method return the information about the square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
