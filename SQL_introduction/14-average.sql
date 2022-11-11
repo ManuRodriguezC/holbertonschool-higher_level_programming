ea81362b84087ded78a693ffd4fe45fca1fb3d9b
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-11 16:46:09 -0500

    Computes the socre acerage of the records in the table

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT SUM(score * quantity) FROM second_table AS average;
+SELECT SUM(score) FROM second_table AS average;
