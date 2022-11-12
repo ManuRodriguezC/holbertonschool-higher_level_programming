8a9f44cfc635abdb8bf930f2648195ba1395187b
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-11 21:24:03 -0500

    Test

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 
+sudo mysqldump -uroot -p hbtn_0c_0 < temperature.sql
+
 SELECT city, avg(value) FROM temperatues GROUP BY city ORDER BY avg(value) DESC;
