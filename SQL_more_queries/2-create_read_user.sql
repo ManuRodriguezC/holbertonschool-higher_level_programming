d7b20a7219270a0f2e3c8262e0dc1681dc4bd4b9
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-14 15:23:11 -0500

    Create a new user and data base with alone permision SELECT

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
-CREATE user_0d_2 hbtn_0d_2 IF NOT EXISTS;
+CREATE database hbtn_0d_2 IF NOT EXISTS;
