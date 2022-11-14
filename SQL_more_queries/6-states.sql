2d3bda9954129c43963e0fa8f6209b62b276d90f
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-14 18:33:34 -0500

    Create a new database and table, an implement auto_incremet for aument the avlue in the case the value is null

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id int NOT NULL auto_intrement,
+    id int NOT NULL auto_increment,
     name varchar(256) NOT NULL,
     PRIMARY KEY(id)
 );
