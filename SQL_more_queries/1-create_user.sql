0a917beb42542b21d589d2759c0507855404281c
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-14 14:52:11 -0500

    Created a new user in the localhost

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates the MySQL server user user_0d_1.
 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS user_0d_1;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRAND OPTION;
