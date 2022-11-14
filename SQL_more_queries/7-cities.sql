0a58bc10a9fbe31df3ae124c8b4c72da8912c3d1
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-14 18:45:48 -0500

    Created a new table and unite the ids with the other table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE cities (
+CREATE TABLE IF NOT EXISTS cities (
     id int NOT NULL AUTO_INCREMENT UNIQUE,
     state_id int NOT NULL, FOREIGN KEY(id) REFERENCES states(id),
     name varchar(256) NOT NULL
