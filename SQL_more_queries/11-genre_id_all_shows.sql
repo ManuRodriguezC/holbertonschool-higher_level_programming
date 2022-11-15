7683222ce7bd0feb3d3e6751e7b8f758c559665d
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-15 08:52:51 -0500

    List of the all elements of two tables

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 --  lists all shows contained in the database hbtn_0d_tvshows.
 
 SELECT s.title, g.genre_id FROM tv_shows s LEFT JOIN tv_show_genres g 
-ON s.id = g.genre_id ORDER BY title, genre_id ASC;
+ON s.id = g.show_id ORDER BY title, genre_id ASC;
