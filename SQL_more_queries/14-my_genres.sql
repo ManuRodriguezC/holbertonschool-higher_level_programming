0c40eb8fe4999db600d4079603a00ec688757ac3
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-15 20:18:58 -0500

    Print the list with the genres that have Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,5 @@
+-- That uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+
 SELECT g.name FROM tv_show_genres sg
 JOIN tv_shows s ON s.id = sg.show_id
 JOIN tv_genres g ON g.id = genre_id
