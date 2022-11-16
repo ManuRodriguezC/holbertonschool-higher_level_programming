afce554fcb150a8d69cab45d434d2ae7a0237ec3
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-15 20:48:00 -0500

    The tv_genres table contains only one record where name = Comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,4 @@
 --  lists all Comedy shows in the database hbtn_0d_tvshows.
 --The tv_genres table contains only one record where name = Comedy
 
-SELECT s.title FROM tv_shows s
-JOIN tv_show_genres sg ON sg.show_id = s.id
-JOIN tv_genres g ON sg.genre_id = g.id
-WHERE g.name = 'Comedy'
-ORDER BY s.title ASC;
+SELECT s.title FROM tv_show_genres sg JOIN tv_shows s ON sg.show_id = s.id JOIN tv_genres g ON sg.genre_id = g.id WHERE g.name = 'Comedy' ORDER BY s.title ASC;
