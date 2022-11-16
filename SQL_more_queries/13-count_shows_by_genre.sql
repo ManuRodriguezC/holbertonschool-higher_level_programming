1a1b7cd6035bdabed22158fdd5bf7e204475bfaf
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-15 19:59:26 -0500

    Uses a count and left join for print all list with the some elements

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 -- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
 
-SELECT g.name AS genre, count(s.genre_id) AS number_of_shows FROM tv_genres g LEFT JOIN tv_show_genres s ON id = genre_id
+SELECT g.name AS genre, count(s.genre_id) AS number_of_shows FROM tv_genres g JOIN tv_show_genres s ON id = genre_id
 GROUP BY g.name
 ORDER BY count(s.genre_id) DESC;
