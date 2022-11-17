646c1d627a9d0c849df2b8af80fcf7fdacdc918f
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-16 21:34:12 -0500

    Print all elemets in the list that start with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module conect to the sql with python,
+open the hbtn databases and print all elemts
+"""
+import MySQLdb
+from sys import argv
+
+
+def date():
+    """
+    This funtion connect to the basedata and
+    print elemets that start with 'N'
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for elemets in cur:
+        if elemets[1].startswith("N"):
+            print(elemets)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    date()
