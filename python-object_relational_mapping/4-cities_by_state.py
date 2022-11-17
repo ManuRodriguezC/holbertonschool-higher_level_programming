863cc8594dd3bd8c1584d0187baa4793d7ebb365
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-17 18:28:28 -0500

    Join two tables in a same database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module conect to the sql with python,
+open the hbtn databases and print all elemts
+"""
+import MySQLdb
+from sys import argv
+
+
+def date():
+    """
+    This funtion connect to the basedata
+    and print elemests join cities and state
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    
+    cur = db.cursor()
+
+    com = "SELECT c.id, c.name, s.name FROM cities c LEFT JOIN \
+            states s ON c.state_id = s.id ORDER BY c.id ASC"
+    """
+    com = "SELECT * FROM cities"
+    """
+    cur.execute(com)
+    for elemets in cur:
+        print(elemets)
+
+    db.close()
+
+
+if __name__ == '__main__':
+    date()
