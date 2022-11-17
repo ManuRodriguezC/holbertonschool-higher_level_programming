00a430180dd90e47f15ed0d5995219cacff3cf45
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-16 21:10:17 -0500

    Created of the file and task 0, uses of the mysqldb for conection sql with python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,21 +6,17 @@ open the hbtn databases and print all elemts
 import MySQLdb
 
 
-def date():
+if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         user="root",
         password="majo0308",
-        database="hbtn_0e_0_usa"
+        database="hbtn_0e_0_usa",
+        port=3306
     )
 
     cur = db.cursor()
 
-    cur.execute("USE hbtn_0e_0_usa")
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for elemets in cur:
         print(elemets)
-
-
-if __name__ == "__main__":
-    date()
