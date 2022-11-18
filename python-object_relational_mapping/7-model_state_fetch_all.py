c036487e5b6a81026d2ac94aea448a233a789d2c
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-18 14:37:13 -0500

    Print all elemetes of teh class State

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module contein and print all elemets of the class"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
@@ -8,7 +9,8 @@ from sys import argv
 def model_state_fetch_all():
     """This function print all elemtes of the class"""
 
-    dir = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+    dir = 'mysql+mysqldb://{}:{}@localhost/{}\
+        '.format(argv[1], argv[2], argv[3])
 
     engine = create_engine(dir)
     Session = sessionmaker(bind=engine)
