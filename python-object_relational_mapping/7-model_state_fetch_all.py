ddf29652adf7f4cdc3cbe6c536eb72cc8cfd26ff
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-18 14:51:12 -0500

    Print all elements of the class State

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,12 +9,16 @@ from sys import argv
 def model_state_fetch_all():
     """This function print all elemtes of the class"""
 
-    dir = 'mysql+mysqldb://{}:{}@localhost/{}\
-        '.format(argv[1], argv[2], argv[3])
+    dir = 'mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(argv[1], argv[2], argv[3])
 
-    engine = create_engine(dir)
+    engine = create_engine(dir, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     states = Session().query(State).order_by(State.id).all()
 
     for elemet in states:
         print(f"{elemet.id}: {elemet.name}")
+
+
+if __name__ == '__main__':
+    model_state_fetch_all()
