da4446094bd0213024f5e1fcb01ebb4754c23a2c
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-18 14:35:32 -0500

    Print all elemetes of teh class State

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+
+def model_state_fetch_all():
+    """This function print all elemtes of the class"""
+
+    dir = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(dir)
+    Session = sessionmaker(bind=engine)
+    states = Session().query(State).order_by(State.id).all()
+
+    for elemet in states:
+        print(f"{elemet.id}: {elemet.name}")
+
+
+if __name__ == '__main__':
+    model_state_fetch_all()
