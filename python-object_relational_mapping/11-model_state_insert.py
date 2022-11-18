d8dc1ad68d12228d1c9216e64bb9776039750b16
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-18 17:07:12 -0500

    Add a new object of the class state in a database

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contein and print all elemets of the class"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def model_state_insert():
+    """ This method add a object state to the basedata"""
+    dir = 'mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(dir, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    State.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    conect = engine.connect()
+    session = Session(bind=conect)
+
+    new_state = State(name="Lousiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+
+
+if __name__ == '__main__':
+    model_state_insert()
