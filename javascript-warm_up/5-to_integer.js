8ea6d95c823e3787b6757a9e34424c6594810267
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-21 13:08:38 -0500

    Convert a variable to integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const process = require('process')
-first = process.argv[2]
+const process = require('process');
+const first = process.argv[2];
 
-number = parseInt(first)
+const number = parseInt(first);
+
 if (isNaN(number) === true) {
-console.log('Not a number')
+  console.log('Not a number');
 } else {
-console.log('My number: ' + number)
+  console.log('My number: ' + number);
 }
