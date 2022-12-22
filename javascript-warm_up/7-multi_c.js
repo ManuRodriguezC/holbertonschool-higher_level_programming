c5266fa77328db9b931bb6c25763d1474583b25d
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-21 20:19:18 -0500

    Task 6 and 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const process = require('process');
 if (process.argv.length === 2) {
-  console.log('Missing number of occurences')
+  console.log('Missing number of occurences');
 }
 if (process.argv.length === 3) {
   if (process.argv[2] > 0) {
     for (let x = 0; x < process.argv[2]; x++) {
-      console.log('C is fun')
+      console.log('C is fun');
     }
-}}
+  }
+}
