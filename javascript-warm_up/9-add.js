1b263b4f276ddfc10d8351fb68ce1d204096b33d
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-22 06:48:43 -0500

    Task 9, add two numbers in the funcition

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+function add (a, b) {
+  console.log(a + b);
+}
+add(first, second);
