03d3e4d039c1b27f276aa48777a8646565aa7de9
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-22 18:31:47 -0500

    Task 4, create more methods

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let text = '';
+    for (let x = 0; x < this.width; x++) {
+      text += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(text);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
