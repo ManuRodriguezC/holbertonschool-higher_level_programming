5e3754c0f1b0437cdcb7ccf9617294a61943e8bc
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-23 08:46:05 -0500

    Task 0 and 1, read an write a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const path = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(path, text, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
