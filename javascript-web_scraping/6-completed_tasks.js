89ccf4338a3301bebde101dd92ed447814603af8
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-23 16:55:46 -0500

    Task 6, computes the number of tasks cpleted by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, date) {
+  if (error) {
+    console.log(error);
+  } else {
+    const objDate = JSON.parse(date);
+    const dates = {};
+    for (let pos = 1; pos <= 10; pos++) {
+      let count = 0;
+      for (let x = 0; x < objDate.length; x++) {
+        if (objDate[x].completed === true && objDate[x].userId === pos) {
+          count += 1;
+        }
+      }
+      dates[pos] = count;
+    }
+    console.log(dates);
+  }
+});
