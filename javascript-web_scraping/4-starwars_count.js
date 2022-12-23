53dd5ab745a895f51c566a20107f43b7cf949cda
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-23 10:54:50 -0500

    Task 4, count the numbers of the participation in films

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request(url, 'utf-8', function (error, response, date) {
     const dateObj = JSON.parse(date).results;
     for (let x = 0; x < dateObj.length; x++) {
       for (let i = 0; i < dateObj[x].characters.length; i++) {
-        const id = dateObj[x].characters[i].split('/')[5]
+        const id = dateObj[x].characters[i].split('/')[5];
         if (id === '18') {
           count++;
         }
