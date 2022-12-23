7f3fccc73fbc2365bc73901a6632f984b7025ef1
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-23 10:14:22 -0500

    Give dates of the api startwars

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request(url, 'utf-8', function (error, response, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    const bodyObj = JSON.parse(data);
+    console.log(bodyObj.title);
+  }
+});
