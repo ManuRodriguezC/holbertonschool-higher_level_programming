87345fc1b39d020861c690964e9469a56070243c
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-23 10:51:27 -0500

    Task 4, count the numbers of the participation in films

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,6 @@ request(url, 'utf-8', function (error, response, date) {
   } else {
     const dateObj = JSON.parse(date).results;
     for (let x = 0; x < dateObj.length; x++) {
-      console.log(dateObj[x].characters);
       for (let i = 0; i < dateObj[x].characters.length; i++) {
         if (dateObj[x].characters[i] === 'https://swapi-api.hbtn.io/api/people/18/') {
           count++;
